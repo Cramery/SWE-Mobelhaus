633f8e9aa1d97309d1d2834bdda79e0d6ee2b174
Cramery <rami-602@hotmail.com>
2018-03-13 08:35:24 +0100

    UI Design
    Erst schritt vom UI design mal uelade

---
+++ SWE_Mobelhaus/src/swe/Exit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swe;
+
+/**
+ *
+ * @author Cramery
+ */
+public class Exit {
+    public static void Exit(){
+        System.exit(0);
+    }
+    
+}
